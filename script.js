e98bda40e2549e0b388468bce8ccdf8f93e624d6
Milenta <marko1milentijevic@gmail.com>
2024-05-08 22:05:23 +0200

    commit

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
-let num = 0;
-let inputN;
+let seacretNum = 0;
+let inputNum;
+let score = 20;
 
 function setNumber() {
-  num = Number(Math.floor(Math.random() * 20 + 1));
+  seacretNum = Number(Math.floor(Math.random() * 20 + 1));
 }
 setNumber();
 function compare() {
-  if (!inputN) {
+  if (!inputNum) {
     document.getElementById("answer").innerText = `You should input a number`;
-  } else if (inputN < num) {
+  } else if (inputNum < seacretNum) {
+    if (score < 1) {
+      // set html text
+      document.querySelector(".message").innerText = `You lost! Play again?`;
+      // set css style
+      document.getElementById("main").style.backgroundColor = "red";
+    } else {
+      // set html text
+      document.getElementById(
+        "answer"
+      ).innerText = `Your guess is ${inputNum}, that is too low, my number is bigger`;
+      document.getElementById("try").innerText = `Try again`;
+      score--;
+      document.querySelector(".score").innerText = `${score}`;
+    }
+  } else if (inputNum > seacretNum) {
+    if (score < 1) {
+      // set html text
+      document.querySelector(".message").innerText = `You lost! Play again?`;
+      // set css style
+      document.getElementById("main").style.backgroundColor = "red";
+    } else {
+      // set html text
+      document.getElementById(
+        "answer"
+      ).innerText = `Your guess is ${inputNum} that is too high, my number is smaller`;
+      document.getElementById("try").innerText = `Try again`;
+    }
+  } else if (inputNum === seacretNum) {
+    // set html text
     document.getElementById(
       "answer"
-    ).innerText = `Your guess is ${inputN}, that is too low, my number is bigger`;
-    document.getElementById("try").innerText = `Try again`;
-    document.getElementById("div2").style.backgroundColor = "red";
-  } else if (inputN > num) {
-    document.getElementById(
-      "answer"
-    ).innerText = `Your guess is ${inputN} that is too high, my number is smaller`;
-    document.getElementById("try").innerText = `Try again`;
-    document.getElementById("div2").style.backgroundColor = "red";
-  } else if (inputN === num) {
-    document.getElementById(
-      "answer"
-    ).innerText = `Your guess ${inputN} spot on, my number is ${num}`;
-    document.getElementById("div2").style.backgroundColor = "rgb(0, 255, 13)";
+    ).innerText = `Your guess ${inputNum} is spot on, my number is also ${seacretNum}`;
+    document.querySelector(".message").innerText = `Play again?`;
+    // set css style
+    document.getElementById("main").style.backgroundColor = "rgb(0, 255, 13)";
   }
 }
 document.getElementById("check").addEventListener("click", function () {
-  inputN = Number(document.getElementById("num").value);
+  inputNum = Number(document.getElementById("num").value);
   compare();
 });
 document.getElementById("reset").addEventListener("click", function () {
   setNumber();
   document.getElementById("answer").innerText = `Your best guess:`;
-  document.getElementById("div2").style.backgroundColor = "blue";
+  document.getElementById("main").style.backgroundColor = "blue";
   document.getElementById("num").value = null;
   document.getElementById("try").innerText = `Try it`;
+  document.querySelector(".score").innerText = `20`;
 });
